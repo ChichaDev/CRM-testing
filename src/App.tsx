7054a8ea6dc647eef4271d16ecd18ff4420f2dc0
ChichaDev <polizhaiofficial@gmail.com>
2023-04-07 15:18:48 +0300

    add redux-toolkit settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import React from "react";
 import { router } from "./routing/routing";
 import { RouterProvider } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
 function App() {
   return (
     <React.StrictMode>
-      <RouterProvider router={router} />
+      <Provider store={store}>
+        <RouterProvider router={router} />
+      </Provider>
     </React.StrictMode>
   );
 }
